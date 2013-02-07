9669d402ace1821fedc6decf3d89521fe4e9f34a
Roman <r.marchant@acfr.usyd.edu.au>
2013-02-07 11:38:11 +1100

    Basic covariance function with periodic behaviour. Basic demos for this cov function.

--- +tacopig/+demos/demo1DPeriodic.m
+++ +tacopig/+demos/demo1DPeriodic.m
@@ -22,78 +22,67 @@ close all; clc;
 %% Ground Truth
 
 %Ideal function where to take samples from
-goal_function = SpaceTimeGoalFunction();
-goal_function.expression = @(s,t)((cos(2*pi*t/2).^2).*(sin(s)./s+exp(s/10)));
+goal_function = GoalFunction();
+goal_function.expression = @(x)(cos(2*pi*x/2));
 
 %Limits for evaluation of the goal function
 %Space (1D) from 0-12 and time from 0-4
-goal_function.limits.low = [0.1 0];
-goal_function.limits.high = [12 4];
+goal_function.limits.low = 0;
+goal_function.limits.high = 4;
 goal_function.noiseStd = 0.1;
 goal_function.noiseMean = 0;
 
 %Get Ground Truth samples from the function to be plotted.
-resolution.s = 0.2;
-resolution.t = 0.05;
-[GT_locations, t_s, t_t] = goal_function.GetEvaluationDomain(resolution);
+numberOfSamples = 500;
+GT_locations = goal_function.GetEvaluationDomain(numberOfSamples);
 GTy = goal_function.GetSample(GT_locations);
 
 %% Plot the Ground Truth
 figure;
-surf(t_s,t_t,reshape(GTy,size(t_s)));
+plot(GT_locations,GTy);
 
 %% Training Data
-N = 50; %Number of training Points.
+N = 10; %Number of training Points.
 
 X = goal_function.GetRandomEvaluationLocations(N);
 y = goal_function.GetNoisySample(X);
 
 %% Plot the Ground Truth with sample points
 figure;
-surf(t_s,t_t,reshape(GTy,size(t_s)));
+plot(GT_locations,GTy);
 hold on,
-plot3(X.s,X.t,y,'o');
+plot(X,y,'o');
 
 %% Set up Gaussian process
 
 % Use a space-time GP regression model:
-GP = tacopig.gp.STGP;
+GP = tacopig.gp.Regressor;
 
 % Plug in the data
+X = X';
+y = y';
 GP.X = X;
 GP.y = y;
 
 % Plug in the components
-GP.MeanFn = tacopig.meanfn.STStationaryMean();
-GP.CovFn   = tacopig.covfn.STSep(tacopig.covfn.SqExp,tacopig.covfn.ExpPeriodic);
+GP.MeanFn = tacopig.meanfn.StationaryMean();
+GP.CovFn   = tacopig.covfn.Sum(tacopig.covfn.ExpPeriodic(),tacopig.covfn.SqExp());
 GP.NoiseFn = tacopig.noisefn.Stationary();
 GP.objective_function = @tacopig.objectivefn.NLML;
 
 GP.solver_function = @anneal;
 
 % Initialise the hyperparameters
-GP.covpar   = 0.5*ones(1,GP.CovFn.npar(size(X,1)));
+GP.covpar   = [1 1 0.5 0.5];%;0.5*ones(1,GP.CovFn.npar(size(X,1)));
 GP.meanpar  = zeros(1,GP.MeanFn.npar(size(X,1)));
 GP.noisepar = 1e-1*ones(1,GP.NoiseFn.npar);
 
-
-%% Before Learning: Query
+%% Query
 GP.solve();
-
-[mf, vf] = GP.query(GT_locations);
-sf  = sqrt(vf);
-
-%% Plot the mean and vf 
-figure;
-surf(t_s,t_t,reshape(mf,size(t_s)));
-hold on
-surf(t_s,t_t,reshape(mf+sf,size(t_s)),'facealpha',0.1);
-surf(t_s,t_t,reshape(mf-sf,size(t_s)),'facealpha',0.1);
-plot3(X.s,X.t,y,'o');
+[mf_p, vf_p] = GP.query(GT_locations);
+sf_p  = sqrt(vf_p);
 
 %% Learn & Query
-%disp('Press any key to begin learning.')
-%pause
 GP.learn();
 GP.solve();
 [mf, vf] = GP.query(GT_locations);
@@ -101,29 +90,16 @@ sf  = sqrt(vf);
 
 %% Plot the mean and vf 
 figure;
-surf(t_s,t_t,reshape(mf,size(t_s)));
+%Learnt model
+f = [mf+2*sf;flipdim(mf-2*sf,1)];
+fill([GT_locations; flipdim(GT_locations,1)], f, [0 0 255]/255, 'EdgeColor', [0 0 255]/255,'edgealpha',0.1);
 hold on
-surf(t_s,t_t,reshape(mf+sf,size(t_s)),'facealpha',0.1);
-surf(t_s,t_t,reshape(mf-sf,size(t_s)),'facealpha',0.1);
-plot3(X.s,X.t,y,'o');
-
-% %% Generate samples from prior and posterior
-% figure; subplot(1,2,1)
-% xstar = linspace(-8,8,100);
-% hold on;
-% for i = 1:5
-%     fstar = GP.sampleprior(xstar);
-%     plot(xstar,fstar, 'color', rand(1,3));
-% end
-% title('Samples from Prior')
-% 
-% subplot(1,2,2)
-% plot(X, y, 'k+', 'MarkerSize', 17)
-% xstar = linspace(-8,8,100);
-% hold on;
-% for i = 1:5
-%     fstar = GP.sampleposterior(xstar, 50+i);
-%     plot(xstar,fstar, 'color', rand(1,3));
-% end
-% title('Samples from Posterior')
-
+plot(GT_locations,mf,'LineWidth',2);
+f_p = [mf_p+2*sf_p;flipdim(mf_p-2*sf_p,1)];
+h = fill([GT_locations; flipdim(GT_locations,1)], f_p, [255 0 0]/255,'FaceColor',[255 0 0]/255, 'EdgeColor', [255 0 0]/255,'edgealpha',0.1);
+hold on
+plot(GT_locations,mf_p,'r','LineWidth',2);
+plot(GT_locations,GTy,'k','LineWidth',2);
+hold on;
+plot(X,y,'rx','LineWidth',2);
+title('After Learning');
